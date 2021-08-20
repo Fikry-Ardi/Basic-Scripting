28c18588acd09e859052ada374c03d9d76147c10
Fikry-Ardi <fikryardi229@gmail.com>
2021-08-21 01:19:10 +0700

    Latihan 6

--- Assets/Script/Latihan4/Karakter.cs
+++ Assets/Script/Latihan4/Karakter.cs
@@ -1,18 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class Karakter : MonoBehaviour
+public class Karakter
 {
-    // Start is called before the first frame update
-    void Start()
+    private string name;
+    private int health;
+    private int damage;
+
+    public string Name
     {
-        
+        get
+        {
+            return name;
+        }
+        set
+        {
+            name = value;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public int Health
     {
-        
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+        }
+    }
+
+    public int Damage
+    {
+        get
+        {
+            return damage;
+        }
+        set
+        {
+            damage = value;
+        }
+    }
+
+    public void MoveLeft()
+    {
+        Debug.Log("Gerak ke kiri");
+    }
+    public void MoveRight()
+    {
+        Debug.Log("Gerak ke kanan");
+    }
+    public void Jump()
+    {
+        Debug.Log("Loncat");
+    }
+    public void Attack()
+    {
+        Debug.Log("Serang");
     }
 }
